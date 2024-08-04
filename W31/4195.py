af9cf085f73f2ade8f5720dd329b921b83545f01
shhwangofficial <shhwangofficial@gmail.com>
2024-08-04 11:10:02 +0900

    Solve 4195 on 8/4

---
+++ W31/4195.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import sys
+
+def find_parent(child):
+    while True:
+        if type(dic[child]) == int:
+            return child
+        else:
+            child = dic[child]
+        
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    F = int(sys.stdin.readline())
+    dic = defaultdict(lambda:1)
+    for i in range(F):
+        a, b = sys.stdin.readline().split()
+        p_a = find_parent(a)
+        p_b = find_parent(b)
+        if p_a == p_b:
+            print(dic[p_a])
+            continue
+        if dic[p_a] >= dic[p_b]:
+            dic[p_a] += dic[p_b]
+            dic[p_b] = p_a
+            print(dic[p_a])
+        else:
+            dic[p_b] += dic[p_a]
+            dic[p_a] = p_b
+            print(dic[p_b])
+

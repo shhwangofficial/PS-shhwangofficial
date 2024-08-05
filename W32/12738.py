942feb918a22a749c765a493cb98c31569e92c16
shhwangofficial <shhwangofficial@gmail.com>
2024-08-05 11:13:09 +0900

    Solve 12738 on 8/5

---
+++ W32/12738.py
@@ -0,0 +1,27 @@
+import sys
+N = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+
+lst = []
+
+for num in nums:
+    if not lst or lst[-1] < num:
+        lst.append(num)
+        continue
+    if lst[0] > num:
+        lst[0] = num
+    else:
+        start = 0
+        end = len(lst)-1
+        temp_idx = -1
+        while start <= end:
+            mid = (start+end)//2
+            if lst[mid] < num:
+                start = mid + 1
+                continue
+            else:
+                temp_idx = mid
+                end = mid -1
+        lst[temp_idx] = num
+
+print(len(lst))

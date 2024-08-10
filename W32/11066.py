5b06e255ca83f44bdd438494b08d8a4e11c537e1
shhwangofficial <shhwangofficial@gmail.com>
2024-08-10 13:01:51 +0900

    Black 11066 on 8/10

--- W32/11066.py
+++ W32/11066.py
@@ -6,12 +6,12 @@ for _ in range(T):
     num = list(map(int, sys.stdin.readline().split()))
     dp = [[0] * len_num for i in range(len_num)]
 
-    s = [0] * (len_num+1)
+    s = [0] * (len_num + 1)
     for i in range(len_num):
         s[i] = s[i - 1] + num[i]
 
     for k in range(1, len_num):
-        for i in range(len_num-k):
+        for i in range(len_num - k):
             j = i + k
             dp[i][j] = min(dp[i][mid] + dp[mid + 1][j] for mid in range(i, j))
             dp[i][j] += s[j] - s[i - 1]

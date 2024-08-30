e1f68659e33e843b8ec74c9407a9a527bef723c8
shhwangofficial <shhwangofficial@gmail.com>
2024-08-30 23:25:17 +0900

    Solve 2352 on 8/30

---
+++ W35/2352.py
@@ -0,0 +1,23 @@
+import sys
+
+N = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+
+lst = []
+for num in nums:
+    if not lst or lst[-1] < num:
+        lst.append(num)
+    elif lst[0] > num:
+        lst[0] = num
+    else:
+        start = 0
+        end = len(lst)-1
+        while start <= end:
+            mid = (start+end)//2
+            if lst[mid] >= num:
+                temp = mid
+                end = mid - 1
+            else:
+                start = mid + 1
+        lst[temp] = num
+print(len(lst))

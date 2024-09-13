6ba45362d18518dfb8157d1585e9ff0168256124
shhwangofficial <shhwangofficial@gmail.com>
2024-09-13 21:14:10 +0900

    Solve 10546 on 9/13

---
+++ W37/10546.js
@@ -0,0 +1,28 @@
+let input = require("fs")
+  .readFileSync(0, "utf-8")
+  .toString()
+  .trim()
+  .split("\n");
+let [N, ...names] = input;
+N = parseInt(N);
+names = names.map((name) => name.trim());
+const dic = {};
+
+for (let i = 0; i < N; i++) {
+  if (dic[names[i]]) {
+    dic[names[i]]++;
+  } else {
+    dic[names[i]] = 1;
+  }
+}
+
+for (let j = N; j < 2 * N - 1; j++) {
+  dic[names[j]]--;
+}
+
+for (key in dic) {
+  if (dic[key] >= 1) {
+    console.log(key);
+    break;
+  }
+}

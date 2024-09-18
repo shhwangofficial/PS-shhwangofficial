d3a4e558fe154ec842c705a7ad3647c17d2a34fd
shhwangofficial <shhwangofficial@gmail.com>
2024-09-18 23:24:21 +0900

    Update format.js to use filePath variable

--- ___format.js
+++ ___format.js
@@ -1,17 +1,15 @@
-// 'js_testcase.txt'
-// 0, 'utf-8'
-
-// readFileSync(0, 'utf-8')
-
 // 입력을 한줄씩 받는게 아니라 한꺼번에 받는다.
 // console.log 는 한번만 실행되게끔 한다.
 
+// 필수
+const filePath = process.platform === 'linux' ? 0 : 'js_testcase.txt';
+
 // N
-let N = Number(require("fs").readFileSync(0, 'utf-8').toString().trim());
+let N = Number(require("fs").readFileSync(filePath, 'utf-8').toString().trim());
 
 // 1 10  -> a = 1, b = 10
 let input1 = require("fs")
-  .readFileSync(0, 'utf-8')
+  .readFileSync(filePath, 'utf-8')
   .toString()
   .trim()
   .split(" ");
@@ -19,22 +17,24 @@ let [a, b] = input1.map(Number);
 
 // 여러줄의 숫자 입력 받을 때
 let input2 = require("fs")
-  .readFileSync(0, 'utf-8')
+  .readFileSync(filePath, 'utf-8')
   .toString()
   .trim()
   .split("\n");
+input2 = input2.map(Number);
 
-// 4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
+// 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
 /* ex)
 3
 110 78 158
 */
 const [n, input3] = require("fs")
-  .readFileSync(0, 'utf-8')
+  .readFileSync(filePath, 'utf-8')
   .toString()
   .trim()
   .split("\n");
 const inputArr3 = input3.split(" ").map(Number);
+
 
 // N개의 0으로 된 일차원 배열
 let dp = Array(N).fill(0);

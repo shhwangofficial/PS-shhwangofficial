66746bb84f94d20efe3bf9a15c4a3de4c3b20380
shhwangofficial <shhwangofficial@gmail.com>
2025-04-17 11:21:32 +0900

    black 4811

--- Y2025/W11~20/W16/4811.py
+++ Y2025/W11~20/W16/4811.py
@@ -9,23 +9,22 @@ med = {
     (1, 0): 1,
 }
 
+
 def dp(tpl):
     W, H = tpl
     if med.get((W, H), 0):
         return med[(W, H)]
     left, right = 0, 0
     if W >= 1:
-        left = dp((W-1, H+1))
+        left = dp((W - 1, H + 1))
     if H >= 1:
-        right = dp((W, H-1))
+        right = dp((W, H - 1))
     med[(W, H)] = left + right
     return med[(W, H)]
+
 
 while True:
     N = int(input())
     if N == 0:
         break
     print(dp((N, 0)))
-
-
-    

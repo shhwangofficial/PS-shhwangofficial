2a5da469cd2894686fce8f9f099f74cdf0aca3e2
shhwang0522 <shhwangofficial@gmail.com>
2025-06-24 00:14:25 +0900

    Solve 10926 on 06/24

---
+++ Y2025/W21~30/W26/Prob10926.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Prob10926 {
+    public static void main(String[] args) throws IOException {
+        // 입력 파일 설정 (백준에서는 제거)
+        // String filePath = "java_input.txt";
+        // BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        String inputString = reader.readLine().trim();
+ 
+        System.out.println(inputString+"??!");
+    }
+}
